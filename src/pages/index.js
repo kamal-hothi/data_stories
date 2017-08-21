289cf5efd63f6225105598e582be6c35638afff8
Kamal Hothi <kamalren@gmail.com>
2017-08-21 19:51:17 +1200

    Initialising new gatsby static site

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () =>
+  <div>
+    <h1>Data Stories</h1>
+    <p>Telling stories with data, using pictures and words</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+
+export default IndexPage
